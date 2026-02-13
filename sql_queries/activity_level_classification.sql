14509a75f7363e81611c4c2a00c961bfe7c741d4
ANDRES RUIZ <anturiam@gmail.com>
2026-02-13 01:30:43 -0500

    Update activity_level_classification.sql

--- sql_queries/activity_level_classification.sql
+++ sql_queries/activity_level_classification.sql
@@ -1,6 +1,7 @@
 /*
 Analysis: Daily Activity Levels
 Description: Classifies daily activity levels based on total daily steps.
+Note: Output labels are in Spanish to match de final report and R analysis files.
 Dataset: dailyActivity_merged
 Tool: Google BigQuery
 Output: Used to generate Figure 1 in the report
