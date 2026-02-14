17ae7db4eeb4ddc44e9cfe502725864d13f6b35d
ANDRES RUIZ <anturiam@gmail.com>
2026-02-14 02:37:47 -0500

    Update figure1_daily_activity_distribution.R

--- r_scripts/figure1_daily_activity_distribution.R
+++ r_scripts/figure1_daily_activity_distribution.R
@@ -1,1 +1,26 @@
+# Project: Bellabeat Case Study
+# Analysis: Daily Activity Levels
+# Description: Generates visualization for Figure 1
 
+library(readr)
+
+activity_level <- read_csv("~/Documents/bellabeat_project/nivel_actividad.csv")
+
+View(activity_level)
+
+library(ggplot2)
+
+ggplot(activity_level, aes(x = nivel_actividad, y = dias, fill = nivel_actividad)) +
+  geom_bar(stat = "identity") +
+  scale_fill_manual(values = c(
+    "Sedentario" = "#E07A5F",
+    "Moderadamente activo" = "#F2CC8F",
+    "Activo" = "#81B29A"
+  )) +
+  labs(
+    title = "Distribución de días según nivel de actividad",
+    x = "Nivel de actividad diaria",
+    y = "Número de días registrados"
+  ) +
+  theme_minimal() +
+  guides(fill = "none")
